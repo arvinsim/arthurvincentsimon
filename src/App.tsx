a1a8065cbf60c5b1e346e40253f23fd6557a0493
Arthur Vincent Simon <developarvin@gmail.com>
2021-09-14 16:27:52 +0800

    Implement initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import ProfilePicture from './profile-picture.jpeg'
+import React from "react";
+import ProfilePicture from "./profile-picture.jpeg";
 
 function App() {
-  return <div className="container mx-auto">
-    <img src={ProfilePicture} className="block object-contain rounded-full" alt={"Profile Picture"} width={200}/>
-    <div>Hi, I am Arthur. I am a Software developer working in Singapore.  I am interested in tech and music.</div>
+  return (
     <div>
-      <div>LinkedIn</div>
-      <div>SoundCloud</div>
-      <div>Code Sandbox</div>
-      <div>Mini Projects</div>
+      <div className="flex flex-col space-y-12 container mx-sm pt-12 m-auto">
+        <img
+          src={ProfilePicture}
+          className="block object-contain rounded-full m-auto"
+          alt={"Profile Picture"}
+          width={200}
+        />
+        <div>
+          <h1 className="text-center text-3xl font-bold">Hi, I am Arthur.</h1>
+          <h2 className="text-center text-2xl">
+            I am a Software developer working in Singapore. I am interested in
+            tech and music.
+          </h2>
+        </div>
+        <div className="text-center">
+          <div>LinkedIn</div>
+          <div>SoundCloud</div>
+          <div>Code Sandbox</div>
+          <div>Mini Projects</div>
+        </div>
+      </div>
     </div>
-  </div>
+  );
 }
 
-export default App
+export default App;
