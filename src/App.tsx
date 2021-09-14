ba500ca43dcb816c0eecf871b9f770d2dbaa089d
Arthur Vincent Simon <developarvin@gmail.com>
2021-09-14 15:45:29 +0800

    Setup Tailwind Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
-import "tailwindcss/tailwind.css"
+import ProfilePicture from './profile-picture.jpeg'
 
 function App() {
-  return <div>App</div>
+  return <div className="container mx-auto">
+    <img src={ProfilePicture} className="block object-contain rounded-full" alt={"Profile Picture"} width={200}/>
+    <div>Hi, I am Arthur. I am a Software developer working in Singapore.  I am interested in tech and music.</div>
+    <div>
+      <div>LinkedIn</div>
+      <div>SoundCloud</div>
+      <div>Code Sandbox</div>
+      <div>Mini Projects</div>
+    </div>
+  </div>
 }
 
 export default App
