221dd167d68fd930667415e3db0d89bc571ad067
Arthur Vincent Simon <developarvin@gmail.com>
2021-12-03 22:32:36 +0800

    Update packages to the latest version

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ProfilePicture from "./profile-picture.jpeg";
 
 function App() {
   return (
-    <div>
+    <div className="bg-black h-screen text-white">
       <div className="flex flex-col space-y-12 container mx-sm pt-12 m-auto max-w-prose">
         <img
           src={ProfilePicture}
@@ -12,15 +12,16 @@ function App() {
           width={200}
         />
         <div>
-          <h1 className="text-center text-3xl font-bold">Hi, I am Arthur.</h1>
-          <h2 className="text-center text-2xl">
-            Software developer working in Singapore focusing on web user
-            interface and experience. I am currently working at Rakuten
-            Singapore. I love music, reading and ideas
+          <h1 className="text-center text-3xl font-bold mb-8">Hi, I am Arthur.</h1>
+          <h2 className="text-center text-1xl">
+            Web software developer. Loves reading books.
+          </h2>
+          <h2 className="text-center text-1xl">
+            Music enthusiast. Exercises for health.
           </h2>
         </div>
-        <div className="text-center">
-          <div>
+        <div className="text-center underline">
+          <div className={"my-4"}>
             <a
               href={
                 "https://www.linkedin.com/in/arthur-vincent-simon-b1307b40/"
@@ -29,9 +30,31 @@ function App() {
               LinkedIn
             </a>
           </div>
-          {/*<div>SoundCloud</div>*/}
-          {/*<div>Code Sandbox</div>*/}
-          {/*<div>Mini Projects</div>*/}
+          <div className={"my-4"}>
+            <a
+                href={"https://github.com/arvinsim"}
+            >
+              Github
+            </a>
+          </div>
+          <div className={"my-4"}>
+            <a
+                href={
+                  "https://stackoverflow.com/users/469921/developarvin?tab=profile"
+                }
+            >
+              Stack Overflow
+            </a>
+          </div>
+          <div className={"my-4"}>
+            <a
+                href={
+                  "https://dev.to/developarvin"
+                }
+            >
+              Dev.to
+            </a>
+          </div>
         </div>
       </div>
     </div>
